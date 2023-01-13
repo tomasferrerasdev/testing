c089ed4081ad83f6f595a374bd2f40e8beaec4a8
Tomas Ferreras <hellotomasdev@gmail.com>
2023-01-13 14:31:30 -0300

    test: implement button test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial color and updates when clicked', () => {
+  render(<App/>)
+  
+  // First assertion
+  const btn = screen.getByRole('button', {name: 'Change to blue'})
+  expect(btn).toHaveStyle({"background-color": 'red'})
+
+  // Second assertion
+  fireEvent.click(btn)
+  expect(btn).toHaveStyle({"background-color": "blue"})
+  expect(btn).toHaveTextContent("Change to red")
 });
+
+
+
