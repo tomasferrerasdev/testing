5a155fec7e2d61c9c80b2409bb83f2cf72d257af
Tomas Ferreras <hellotomasdev@gmail.com>
2023-01-13 15:01:33 -0300

    test: implement button and checkbox test

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import './App.css';
 function App() {
   const [buttonColor, setButtonColor] = useState('red')
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red'
+  
+  const [isChecked, setIsChecked] = useState(false)
+  console.log(isChecked)
+
   return (
     <div className="App">
       <button
         onClick={() => setButtonColor(newButtonColor)}
         style={{backgroundColor: buttonColor}}
+        disabled={isChecked}
       >
         Change to {newButtonColor}
       </button>
+      <input type="checkbox" id='disable-button-checkbox' defaultChecked={isChecked} onClick={() => setIsChecked(!isChecked)} />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -15,4 +15,27 @@ test('button has correct initial color and updates when clicked', () => {
 });
 
 
+test('checkbox should be unchecked', () => {
+  render(<App/>)
+  // Check btn start out enabled
+  const btn = screen.getByRole('button', {name: 'Change to blue'})
+  expect(btn).toBeEnabled()
+
+  // Checkbox starts out unchecked
+  const checkbox = screen.getByRole('checkbox')
+  expect(checkbox).not.toBeChecked()
+})
+
+test('checkbox disables button on first click and enables on second one', () => {
+  render(<App/>)
+  const checkbox = screen.getByRole('checkbox')
+  const btn = screen.getByRole('button')
+  
+  fireEvent.click(checkbox)
+  expect(btn).toBeDisabled()
+
+  fireEvent.click(checkbox)
+  expect(btn).not.toBeDisabled()
+})
+
 
